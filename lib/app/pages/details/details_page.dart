9bbca5242b47d0132fea9072d6f982be6cd732bc
Felipe Sales <soufeliposales@gmail.com>
2022-07-08 20:22:16 -0300

    creating utils for utilities in app

--- lib/app/pages/details/details_page.dart
+++ lib/app/pages/details/details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game/app/core/text/app_text_styles.dart';
+import 'package:game/app/utils/constants.dart';
 import 'package:game/app/widgets/custom_divider.dart';
 
 class DetailsPage extends StatelessWidget {
@@ -41,7 +42,7 @@ class DetailsPage extends StatelessWidget {
                 ),
                 const SizedBox(height: 16),
                 Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  Constants.loremIpsum,
                   style: AppTextStyles.gameDetails,
                 ),
               ],
